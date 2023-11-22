a196055406f560802adad8a061bcbeabe0cdb78c
Вакаускас Витас <ivitasvakayskas@gmail.com>
2023-11-22 11:03:53 +0300

    added env_name variable to environment

--- src/bronfood/settings.py
+++ src/bronfood/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
+ENV_NAME = os.getenv('ENV_NAME')
+
 ALLOWED_HOSTS = []
 
 
