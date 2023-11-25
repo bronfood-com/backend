2b8368f28a68b141fcbda9b49b1e0794f84f67f4
LukoninDmitryPy <liondmitry4@gmail.com>
2023-11-25 17:44:49 +0300

    add some tests

---
+++ src/tests/api/restaurant/test_restaurants.py
@@ -0,0 +1,21 @@
+from django.test import TestCase, Client
+
+
+class UrlTests(TestCase):
+    def test_get_restaurants(self):
+        """
+        Тест проверки получения статуса 200 при get запросе
+        на адрес '/restaurants/'
+        """
+        guest_client = Client()
+        response = guest_client.get("/restaurants/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_menu(self):
+        """
+        Тест проверки получения статуса 200 при get запросе
+        на адрес '/menus/'
+        """
+        guest_client = Client()
+        response = guest_client.get("/menus/")
+        self.assertEqual(response.status_code, 200)
