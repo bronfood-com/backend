a541bb49698947627d48adb7fe0f924d9857af7b
Solodnikov <solodnikov@yandex.ru>
2023-11-27 18:04:16 +0300

    customuser example

--- src/bronfood/core/client/models.py
+++ src/bronfood/core/client/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.validators import UnicodeUsernameValidator
-from django.utils import timezone
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, AbstractUser
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 
 
 class CustomUserManager(BaseUserManager):
