efb4935739c16f6f38a8cbe8b3c0d3df7720b7d7
Solodnikov <solodnikov@yandex.ru>
2023-12-10 13:41:11 +0300

    Login/logout session client

--- src/bronfood/core/client/serializers.py
+++ src/bronfood/core/client/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Client
+from django.contrib.auth import authenticate
 
 
 # class OwnerSerializer(serializers.ModelSerializer):
@@ -30,19 +31,42 @@ class ClientSerializer(serializers.ModelSerializer):
         return user
 
 
-# class UpdateClientSerializer(serializers.ModelSerializer):
-#     """
-#     Сериализатор для обновления объекта клиента.
-#     """
-#     password = serializers.CharField(required=False)
-#     email = serializers.EmailField(required=False)
+class UpdateClientSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для обновления объекта клиента.
+    """
+    password = serializers.CharField(required=False)
+    email = serializers.EmailField(required=False)
 
-#     class Meta:
-#         model = Client
-#         fields = ['password', 'email']
+    class Meta:
+        model = Client
+        fields = ['password', 'email']
 
-#     def update(self, instance, validated_data):
-#         password = validated_data.pop('password', None)
-#         if password:
-#             instance.set_password(password)
-#         return super().update(instance, validated_data)
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(password)
+        return super().update(instance, validated_data)
+
+
+class ClientLoginSerializer(serializers.Serializer):
+    """
+    Сериализатор для входа клиента.
+    """
+    phone = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        phone = data.get('phone')
+        password = data.get('password')
+
+        if phone and password:
+            user = authenticate(phone=phone, password=password)
+            if user:
+                data['user'] = user
+            else:
+                raise serializers.ValidationError('Unable to log in with provided credentials.')
+        else:
+            raise serializers.ValidationError('Both phone and password are required fields.')
+
+        return data
--- src/bronfood/core/client/views.py
+++ src/bronfood/core/client/views.py
@@ -1,11 +1,12 @@
 from rest_framework.generics import CreateAPIView
 from .models import Client
-from .serializers import Client, ClientSerializer
+from .serializers import Client, ClientSerializer, ClientLoginSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.contrib.auth import authenticate, login, logout
 
 
 # class CreateClient(CreateAPIView):
@@ -40,3 +41,58 @@ class ClientAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ClientLoginView(APIView):
+
+    serializer_class = ClientLoginSerializer
+
+    @swagger_auto_schema(
+        tags=['client_tag'],
+        operation_summary='Create login',
+        request_body=ClientLoginSerializer(),
+        responses={
+            status.HTTP_200_OK: "{'message': 'Logged in successfully'}",
+            status.HTTP_401_UNAUTHORIZED: 'Invalid credentials',
+            status.HTTP_400_BAD_REQUEST: 'Invalid data'
+        }
+    )
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            phone = serializer.validated_data.get('phone')
+            password = serializer.validated_data.get('password')
+
+            user = authenticate(request=request, phone=phone, password=password)
+
+            if user:
+                login(request, user)
+                return Response(
+                    {'message': 'Logged in successfully'},
+                    status=status.HTTP_200_OK)
+            else:
+                return Response(
+                    {'message': 'Invalid credentials'},
+                    status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ClientLogoutView(APIView):
+    @swagger_auto_schema(
+        tags=['client_tag'],
+        operation_summary='Logout',
+        # request_body=ClientLoginSerializer(),
+        responses={
+            status.HTTP_200_OK: "{'message': 'Logged out successfully'}",
+            status.HTTP_401_UNAUTHORIZED: 'User not authenticated',
+        }
+    )
+    def post(self, request):
+        if request.user.is_authenticated:
+            logout(request)
+            return Response(
+                {'message': 'Logged out successfully'},
+                status=status.HTTP_200_OK)
+        return Response(
+            {'message': 'User not authenticated'},
+            status=status.HTTP_401_UNAUTHORIZED)
