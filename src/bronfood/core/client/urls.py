99ff209520b9ce7cfb0e5f3bcb91aedc13e03386
Solodnikov <solodnikov@yandex.ru>
2023-12-10 13:41:11 +0300

    Login/logout session client

--- src/bronfood/core/client/urls.py
+++ src/bronfood/core/client/urls.py
@@ -1,13 +1,16 @@
 # Создать эдпоинты для клиента в api/client (views, serializers, urls)
 # интегрировать их в основное приложение проекта (пока пустые)
-
-from django.urls import path
-from .views import ClientAPIView
+from rest_framework import urls
+from django.urls import path, include
+from .views import ClientAPIView, ClientLoginView, ClientLogoutView
 
 app_name = 'client'
 
 urlpatterns = [
     path('registration/', ClientAPIView.as_view(), name='index'),
+    path('', include('rest_framework.urls')),
+    path('my_login/', ClientLoginView.as_view(), name='my_login'),
+    path('my_logout/', ClientLogoutView.as_view(), name='my_logout')
     # path('login/', views.group_posts, name='group_posts'),
     # path('logout/',views.group_list, name='group_list'),
     # path('get_detail/',views.group_list, name='group_list'),
