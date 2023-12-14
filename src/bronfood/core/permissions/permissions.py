4b54b352139e695480ff2dffd1b9577ca13c8b12
syberflea <syberlea@gmail.com>
2023-12-14 15:14:02 +0300

    Create custom permissions for app roles: IsAuthenticatedClient IsAuthenticatedRestaurantOwner IsAuthenticatedRestaurantAdmin

---
+++ src/bronfood/core/permissions/permissions.py
@@ -0,0 +1,41 @@
+from rest_framework import permissions
+
+
+class IsAuthenticatedRestaurantOwner(permissions.BasePermission):
+    """ Класс определеня прав для владельца ресторана."""
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user == obj.author
+            or request.user.role.OWMER
+        )
+
+
+class IsAuthenticatedRestaurantAdmin(permissions.BasePermission):
+    """ Класс определеня прав для администратора ресторана."""
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.role.RESTAURANT_ADMIN
+        )
+
+
+class IsAuthenticatedClient(permissions.BasePermission):
+    """ Класс определеня прав для клиента ресторана/заведения."""
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.role.CLIENT
+        )
