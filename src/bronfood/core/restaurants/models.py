e5baf629e888bdb8e8e32fec278248d23ac7accc
syberflea <syberlea@gmail.com>
2023-12-25 10:22:09 +0300

    Add restaurant model

--- src/bronfood/core/restaurants/models.py
+++ src/bronfood/core/restaurants/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import UniqueConstraint
+from bronfood.core.client.models import Client
 
 
 class Complement(models.Model):
@@ -19,11 +21,20 @@ class Complement(models.Model):
 
 class Dish(models.Model):
     """Блюдо."""
+    class SizeOfDish(models.TextChoices):
+        SMALL = "S"
+        MEDIUM = "M"
+        LARGE = "L"
     name = models.CharField('Название блюда', max_length=255)
     description = models.CharField('Описание', max_length=255, null=True)
     price = models.PositiveIntegerField('Цена')
     image = models.ImageField('Изображение блюда', upload_to='pics')
-    size = models.CharField('Размер блюда', max_length=255)
+    size = models.CharField(
+        'Размер блюда',
+        max_length=2,
+        choices=SizeOfDish.choices,
+        default=SizeOfDish.L
+    )
     wait = models.PositiveIntegerField('Время ожидания')
     tags = models.CharField('Теги', max_length=50)
     complement = models.ForeignKey(
@@ -45,12 +56,13 @@ class Menu(models.Model):
         blank=True
     )
     dishes = models.ManyToManyField(Dish, verbose_name='Блюда')
+    category = models.CharField('Категория меню', max_length=255)
 
 
 class Restaurant(models.Model):
     """Модель ресторана."""
     class TypeOfShop(models.TextChoices):
-        FASTFOOD = "FF",
+        FASTFOOD = "FF"
         CAFE = "CA"
         COFFESHOP = "CS"
     name = models.CharField('Название', max_length=255)
@@ -74,3 +86,61 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Favorite(models.Model):
+    '''Избранный рецепт'''
+    user = models.ForeignKey(
+        Client,
+        related_name='favorites',
+        on_delete=models.CASCADE,
+        verbose_name='Клиент'
+    )
+    dish = models.ForeignKey(
+        Dish,
+        related_name='favorites',
+        on_delete=models.CASCADE,
+        verbose_name='Блюдо'
+    )
+
+    class Meta:
+        verbose_name = 'Избранное блюдо'
+        verbose_name_plural = 'Избранные блюда'
+        constraints = [
+            UniqueConstraint(
+                fields=['user', 'recipe'],
+                name="unique_favorite"
+            )
+        ]
+
+        def __str__(self):
+            return f"{self.user.name} likes {self.recipe.name}"
+
+
+class ShopingCart(models.Model):
+    '''Список покупок'''
+    user = models.ForeignKey(
+        Client,
+        related_name='shopingcarts',
+        on_delete=models.CASCADE,
+        verbose_name='Клиент'
+    )
+    dish = models.ForeignKey(
+        Dish,
+        related_name="shopingcarts",
+        on_delete=models.CASCADE,
+        verbose_name='Блюдо'
+    )
+
+    class Meta:
+        verbose_name = 'Блюдо в корзине'
+        verbose_name_plural = 'Блюда в корзине'
+        constraints = [
+            UniqueConstraint(
+                fields=['user', 'recipe'],
+                name="unique_shopingcart"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user}'s {self.dish}"
