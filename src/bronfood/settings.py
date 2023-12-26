f020fdde479627f5f27907849e4349759f225f6a
Nikita <vomerff@gmail.com>
2023-12-26 16:33:05 +0700

    create app payment_requisites

--- src/bronfood/settings.py
+++ src/bronfood/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'bronfood.core.client.apps.ClientConfig',
+    'payment_requisites.apps.PaymentRequisitesConfig',
     'bronfood.core.useraccount.apps.UseraccountConfig',
     'bronfood.api.apps.ApiConfig',
     'bronfood.core.phone.apps.PhoneConfig',
---
+++ src/payment_requisites/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PaymentRequisitesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'payment_requisites'
