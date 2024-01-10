86c0ab9d450d2d3d2e524f2c4664fd71d3ffddb7
syberflea <syberlea@gmail.com>
2024-01-10 20:14:44 +0300

    Made the rights more specific

--- src/bronfood/core/permissions/permissions.py
+++ src/bronfood/core/permissions/permissions.py
@@ -4,32 +4,40 @@ from rest_framework import permissions
 class IsAuthenticatedRestaurantOwner(permissions.BasePermission):
     """ Класс определеня прав для владельца ресторана."""
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return True
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.role.OWNER
+        )
 
     def has_object_permission(self, request, view, obj):
-        return request.method in permissions.SAFE_METHODS\
-            or request.user == obj.author\
+        return (
+            request.method in permissions.SAFE_METHODS
             or request.user.role.OWNER
+        )
 
 
 class IsAuthenticatedRestaurantAdmin(permissions.BasePermission):
     """ Класс определеня прав для администратора ресторана."""
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return True
+        return (
+            request.method in ('GET', 'PUT', 'UPDATE')
+            and request.user.role.RESTAURANT_ADMIN
+        )
 
     def has_object_permission(self, request, view, obj):
-        return request.method in permissions.SAFE_METHODS or\
-            request.user.role.RESTAURANT_ADMIN
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.role.RESTAURANT_ADMIN
+        )
 
 
 class IsAuthenticatedClient(permissions.BasePermission):
     """ Класс определеня прав для клиента ресторана/заведения."""
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return True
+        return True
 
     def has_object_permission(self, request, view, obj):
-        return request.method in permissions.SAFE_METHODS\
+        return (
+            request.method in permissions.SAFE_METHODS
             or request.user.role.CLIENT
+        )
