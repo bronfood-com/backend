7e098af85ea15a288c18a5928d78b032018d74d3
solodnikov <solodnikov@yandex.ru>
2024-01-17 02:33:15 +0300

    token version 1

--- src/bronfood/api/client/urls.py
+++ src/bronfood/api/client/urls.py
@@ -1,24 +1,26 @@
 from django.urls import path, re_path
 from djoser.views import TokenDestroyView
 
-from .views import (ClientChangePasswordConfirmationView,
-                    ClientChangePasswordRequestView, ClientChangePasswordView,
+from .views import (ClientChangePasswordView,
                     ClientConfirmationView, ClientProfileView,
-                    ClientRegistrationView, CustomTokenCreateView)
+                    ClientRegistrationView, CustomTokenCreateView,
+                    ClientDataToRegistrationView,  # перенаправление данных
+                    ClientDataToProfileView,
+                    )
 
 app_name = 'client'
 
 urlpatterns = [
     path('profile/', ClientProfileView.as_view(), name='profile'),
-    path('change_password/request/',
-         ClientChangePasswordRequestView.as_view(),
-         name='change_password_request'),
-    path('change_password/confirmation/',
-         ClientChangePasswordConfirmationView.as_view(),
-         name='change_password_confirmation'),
+#     path('change_password/request/',
+#          ClientChangePasswordRequestView.as_view(),
+#          name='change_password_request'),
+#     path('change_password/confirmation/',
+#          ClientChangePasswordConfirmationView.as_view(),
+#          name='change_password_confirmation'),
     path('change_password/',
          ClientChangePasswordView.as_view(),
-         name='change_password_complete'),
+         name='change_password'),
     path('confirmation/',
          ClientConfirmationView.as_view(),
          name='confirmation'),
@@ -33,4 +35,9 @@ urlpatterns += [
     re_path(r"^signout/?$", TokenDestroyView.as_view(), name="signout"),
     re_path(r"^signin/?$", CustomTokenCreateView.as_view(), name="signin"),
 ]
-    
+
+# Redirection
+urlpatterns += [
+    path('data_to_signup/', ClientDataToRegistrationView.as_view(), name="data_to_signup"),
+    path('data_to_profile/', ClientDataToProfileView.as_view(), name="data_to_profile"),
+]
--- src/bronfood/settings.py
+++ src/bronfood/settings.py
@@ -89,20 +89,14 @@ WSGI_APPLICATION = 'bronfood.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE'),
-#         'NAME': os.getenv('POSTGRES_DB'),
-#         'USER': os.getenv('POSTGRES_USER'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT')
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 # Password validation
@@ -157,8 +151,6 @@ SMS_SETTINGS = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        # 'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication', # Token
     ],
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
