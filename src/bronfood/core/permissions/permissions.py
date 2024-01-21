40ee8b18279f0c925ca857f1d39c8705e3ac7e53
syberflea <syberlea@gmail.com>
2024-01-21 10:15:23 +0300

    Fix permissions according Qerced advices

--- src/bronfood/core/permissions/permissions.py
+++ src/bronfood/core/permissions/permissions.py
@@ -4,40 +4,31 @@ from rest_framework import permissions
 class IsAuthenticatedRestaurantOwner(permissions.BasePermission):
     """ Класс определеня прав для владельца ресторана."""
     def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.role.OWNER
-        )
+        return request.user.is_authenticated and request.user.role == 'owner'
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.role.OWNER
-        )
+        return obj.user == request.user
 
 
 class IsAuthenticatedRestaurantAdmin(permissions.BasePermission):
     """ Класс определеня прав для администратора ресторана."""
     def has_permission(self, request, view):
         return (
-            request.method in ('GET', 'PUT', 'UPDATE')
-            and request.user.role.RESTAURANT_ADMIN
+            request.user.is_authenticated
+            and request.user.role == 'restaurant_admin'
         )
 
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.role.RESTAURANT_ADMIN
+            request.method in ('GET', 'PUT', 'UPDATE')
+            and obj.user == request.user
         )
 
 
 class IsAuthenticatedClient(permissions.BasePermission):
     """ Класс определеня прав для клиента ресторана/заведения."""
     def has_permission(self, request, view):
-        return True
+        return request.user.is_authenticated and request.user.role == 'client' 
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.role.CLIENT
-        )
+        return obj.user == request.user
