3c1d10b81163894fa753c721a1006d7089f1887c
solodnikov <solodnikov@yandex.ru>
2024-02-19 21:06:36 +0300

    clean apps

--- src/bronfood/core/payment_requisites/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PaymentRequisitesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bronfood.core.payment_requisites'
