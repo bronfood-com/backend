73fd5afc2f56ba031afa218797c1d271fcdf78c2
syberflea <syberlea@gmail.com>
2024-02-21 21:14:48 +0300

    Fix unknown class CustomUser

--- src/bronfood/core/auth_backends.py
+++ src/bronfood/core/auth_backends.py
@@ -1,7 +1,9 @@
 # custom auth_backends.py
 
-from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import BaseBackend
+
+User = get_user_model()
 
 
 class PhoneBackend(BaseBackend):
@@ -9,23 +11,37 @@ class PhoneBackend(BaseBackend):
     Работает для клиента и владельца ресторана, проверка пароля и телефона.
     """
 
-    def authenticate(self, request, username=None, password=None):
-        User = get_user_model()
+    def authenticate(self, request, phone, username=None, password=None):
+        try:
+            user = User.objects.get(phone=phone)
+            if user.check_password(password):
+                return user
+            else:
+                return None
+        except user.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
+
+class UsernameBackend(BaseBackend):
+    """Работает для админа ресторана, проверка пароля и юзернэйм."""
+    def authenticate(self, request, phone, username=None, password=None):
         try:
             user = User.objects.get(username=username)
             if user.check_password(password):
                 return user
             else:
                 return None
-        except CustomUser.DoesNotExist:
+        except user.DoesNotExist:
             return None
 
     def get_user(self, user_id):
         try:
-            return CustomUser.objects.get(pk=user_id)
-        except CustomUser.DoesNotExist:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
             return None
-
-
-class UsernameBackend(BaseBackend):
-    """Работает для админа ресторана, проверка пароля и юзернэйм."""
