86e8efa36c3d5c3cd307963da2d9be9a8625d4a5
syberflea <syberlea@gmail.com>
2024-03-03 15:53:34 +0300

    Get back Phone app

---
+++ src/bronfood/core/phone/utils.py
@@ -0,0 +1,14 @@
+from django.utils.crypto import get_random_string
+
+from bronfood.core.phone.models import PhoneSmsOtpVerification, SmsStatus
+
+
+def generate_otp(phone_number):
+    while True:
+        code = get_random_string(length=4, allowed_chars='0123456789')
+        if not PhoneSmsOtpVerification.objects.filter(
+            sms_status=SmsStatus.PENDING,
+            code=code,
+            phone_number=phone_number,
+        ).exists():
+            return code
