19031a6e8465542df0c890cf1c5a85d6710dc57f
Alexander Solodnikov <solodnikov@yandex.ru>
2024-03-09 16:18:06 +0300

    bug fix

--- src/bronfood/api/restaurant_owner/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from bronfood.core.restaurant_owner.models import RestaurantOwner
-
-
-class RestaurantOwnerSerializer(serializers.ModelSerializer):
-    """Сериализатор владельца заведения."""
-    class Meta:
-        model = RestaurantOwner
-        fields = '__all__'
