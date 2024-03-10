d8f37aee6a0d41451bf932aa377984e29cafb191
syberflea <syberlea@gmail.com>
2024-03-10 11:29:36 +0300

    Fix  F811 redefinition of unused 'healthcheck' from line 19 in .\backend\src\bronfood\urls.py

--- src/bronfood/urls.py
+++ src/bronfood/urls.py
@@ -15,14 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path, include
-from bronfood.api.views import healthcheck
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
+from django.urls import include, path, re_path
 from drf_yasg import openapi
-from bronfood.api.views import CustomTokenCreateView
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
-from bronfood.api.views import healthcheck
+from bronfood.api.views import CustomTokenCreateView, healthcheck
 
 urlpatterns = [
     path('admin/', admin.site.urls),
