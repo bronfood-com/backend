5b807a01317203cf51bc762d76025fab8eae84b1
Pavel Okhrim <paokhrim@gmail.com>
2024-06-24 11:41:53 +0300

    Обновлены методы удаления блюда из корзины и очистки корзины. На ревью.

--- src/bronfood/api/restaurants/views.py
+++ src/bronfood/api/restaurants/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
